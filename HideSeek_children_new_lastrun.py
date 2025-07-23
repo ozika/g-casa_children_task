c01dfb9b442c6d41a1994307521bbcfa16aa5422
exp-gcasa-study <exp-gcasa-study@mpib-berlin.mpg.de>
2025-07-23 10:41:28 +0200

    V04LKM

--- HideSeek_children_new_lastrun.py
+++ HideSeek_children_new_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.5),
-    on July 17, 2025, at 17:10
+    on July 23, 2025, at 08:59
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
